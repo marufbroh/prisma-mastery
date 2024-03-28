35a0a30a15bb14132c536b3b9f36e8d7895a5d5e
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-03-28 10:34:38 +0600

    Sum, Count, Maximum, Minimum Aggregation

--- src/aggregates.ts
+++ src/aggregates.ts
@@ -4,13 +4,47 @@ const prisma = new PrismaClient();
 
 const aggregates = async () => {
 
+    // find average age
     const avgAge = await prisma.user.aggregate({
         _avg: {
             age: true
         }
     });
 
-    console.log(avgAge);
+    // find sum of age
+    const sumOfAge = await prisma.user.aggregate({
+        _sum: {
+            age: true
+        }
+    });
+
+    // find count on num filed
+    const countOfAge = await prisma.user.aggregate({
+        _count: {
+            age: true
+        }
+    });
+
+    // find number of record
+    const countData = await prisma.user.count();
+
+
+    // find max age
+    const maxAge = await prisma.user.aggregate({
+        _max: {
+            age: true
+        }
+    })
+
+
+    // find min age
+    const minAge = await prisma.user.aggregate({
+        _min: {
+            age: true
+        }
+    })
+
+    console.log(minAge);
 
 };
 
